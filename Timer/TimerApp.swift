3302354316ff67385dda1be9b2c89866f362a133
jmbae <jmbae@codegrove.co.kr>
2024-04-09 20:52:43 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Jungman Bae on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
